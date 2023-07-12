a1f67e98a723dc5dc7f5e7d9b697bf1d4b82697e
Leah Genkinger <genkinger@protonmail.com>
2023-07-12 11:18:08 +0200

    added simple average normalization

---
+++ src/frontend.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+from stitching import do_stitching, StitcherConfig
+from preprocessing.vignetting_correction import do_vignetting_correction
+import argparse
+
+
+class Frontend(object):
+    def __init__(self):
+        main_parser = argparse.ArgumentParser(prog="NerveStitcher_R2", description="Frontend for NerveStitcher_R2")
+        main_parser.add_argument("command")
+        arguments = main_parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, arguments.command):
+            main_parser.print_help()
+            exit(1)
+
+        getattr(self, arguments.command)()
+
+    def vignetting_correction(self):
+        parser = argparse.ArgumentParser(
+            description="Runs Vignetting Correction on the images in the input_directory and stores the result in output_directory")
+        parser.add_argument("-i", "--input_directory", required=True)
+        parser.add_argument("-o", "--output_directory", required=True)
+        parser.add_argument("-r", "--reference", required=True,
+                            help="Path to a single reference image that is used to compute a reference histogram")
+        arguments = parser.parse_args(sys.argv[2:])
+
+        if not os.path.exists(arguments.output_directory):
+            os.makedirs(arguments.output_directory)
+
+        do_vignetting_correction(arguments.referece, arguments.input_directory, arguments.output_directory)
+
+    def stitch(self):
+        parser = argparse.ArgumentParser(
+            description="Runs the final stitching on the given input directory"
+        )
+        parser.add_argument("-i", "--input_directory", required=True)
+        parser.add_argument("-o", "--final_stitch_directory", required=True)
+        parser.add_argument("-s", "--intermediate_stitch_directory", required=True)
+        parser.add_argument("-m", "--matching_threshold", required=False, default=0.80)
+        parser.add_argument("-k", "--keypoint_threshold", required=False, default=0.005)
+        parser.add_argument("-n", "--nms_radius", required=False, default=4)
+
+        arguments = parser.parse_args(sys.argv[2:])
+
+        stitcher_config = StitcherConfig(input_image_directory=arguments.input_directory,
+                                         final_stitch_output_directory=arguments.final_stitch_directory,
+                                         intermediate_stitch_output_directory=arguments.intermediate_stitch_directory,
+                                         match_threshold=arguments.matching_threshold,
+                                         nms_radius=arguments.nms_radius,
+                                         keypoint_threshold=arguments.keypoint_threshold)
+        do_stitching(stitcher_config)
+
+
+Frontend()
---
+++ src/preprocessing/averaging.py
@@ -0,0 +1,31 @@
+import os.path
+
+import cv2
+from os.path import join, normpath, basename, exists
+from os import listdir, makedirs
+
+import numpy
+import numpy as np
+
+def normalize(x):
+    return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+def apply_average_normalization(input_directory, output_directory):
+    image_paths = [join(normpath(input_directory), filename) for filename in listdir(input_directory)]
+    image_tuples = [(basename(image_path), cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)/255.0) for image_path in image_paths]
+    images = np.array(list(map(lambda image_tuple: image_tuple[1], image_tuples)))
+    average_image = np.average(images, axis=0)
+    normalized_images = list(
+        map(lambda image: (image[0], normalize(np.divide(image[1], average_image))), image_tuples))
+    cv2.imshow("avg", normalized_images[10][1])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    if not exists(output_directory):
+        makedirs(output_directory)
+
+    for image_name, normalized_image in normalized_images:
+        cv2.imwrite(join(output_directory, image_name), (normalized_image*255.0).astype(np.ubyte))
+
+
+apply_average_normalization("../../data/test/stitch_img2", "../../data/test/stitch_img2/avg_corr")
